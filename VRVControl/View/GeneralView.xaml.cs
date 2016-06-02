46df609f45846c7313f638327cbf1451095b646e
Bcoolie <vuletic23@gmail.com>
2016-06-02 15:08:42 +0200

    Code refactoring

--- VRVControl/View/GeneralView.xaml.cs
+++ VRVControl/View/GeneralView.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using NAudio.CoreAudioApi;
 using System.Speech.Recognition;
+using VRVControl.ViewModel;
 
 namespace VRVControl.View
 {
@@ -32,113 +33,14 @@ namespace VRVControl.View
         public GeneralView()
         {
             InitializeComponent();
-            this.Loaded += new RoutedEventHandler(GeneralView_Loaded);            
-        }       
+            this.DataContext = new GeneralViewModel();
+            this.Loaded += new RoutedEventHandler(GeneralView_Loaded);
+        }
 
         private void GeneralView_Loaded(object sender, RoutedEventArgs e)
         {
             MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
             volumeSlider.Value = defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar;
-        }
-
-        private void Mute()
-        {
-            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            if (defaultDevice.AudioEndpointVolume.Mute)
-                defaultDevice.AudioEndpointVolume.Mute = false;
-            else
-                defaultDevice.AudioEndpointVolume.Mute = true;
-        }
-
-        private void VolDown()
-        {
-            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            defaultDevice.AudioEndpointVolume.VolumeStepDown();
-            volumeSlider.Value = defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar;
-            volumeProgressBar.Value = volumeSlider.Value;
-        }
-
-        private void VolUp()
-        {
-            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            defaultDevice.AudioEndpointVolume.VolumeStepUp();
-            volumeSlider.Value = defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar;
-            volumeProgressBar.Value = volumeSlider.Value;
-        }
-
-        void bntIncVol(object sender, RoutedEventArgs e)
-        {
-            VolUp();
-        }
-
-        void bntDecVol(object sender, RoutedEventArgs e)
-        {
-            VolDown();
-        }
-
-        private void btnMute(object sender, RoutedEventArgs e)
-        {
-            Mute();
-        }              
-
-        private void btnStartVoiceControl_Click(object sender, RoutedEventArgs e)
-        {
-            btnStartVoiceControl.IsEnabled = false;
-            btnStopVoiceControl.IsEnabled = true;
-            clist.Add(new string[] { "Increase sound", "Decrease sound", "Mute", "Close" });
-            Grammar gr = new Grammar(new GrammarBuilder(clist));
-
-            try
-            {
-                sre.RequestRecognizerUpdate();
-                sre.LoadGrammar(gr);
-                sre.SpeechRecognized += sre_SpeechRecognized;
-                sre.SetInputToDefaultAudioDevice();
-                sre.RecognizeAsync(RecognizeMode.Multiple);
-
-            }
-            catch (Exception ex)
-            {
-
-                MessageBox.Show(ex.Message, "Error");
-            }
-        }
-
-        private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-
-            switch (e.Result.Text.ToString())
-            {
-                case "Increase sound":
-                    defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar += (float)0.10;
-                    break;
-                case "Decrease sound":
-                    defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar -= (float)0.10;
-                    break;
-                case "Mute":
-                    Mute();
-                    break;
-                case "Close":
-                    Application.Current.Shutdown();
-                    break;
-            }
-
-        }
-
-        private void btnStopVoiceControl_Click(object sender, RoutedEventArgs e)
-        {
-            sre.RecognizeAsyncStop();
-            btnStartVoiceControl.IsEnabled = true;
-            btnStopVoiceControl.IsEnabled = false;
-        }
-
-        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = (float)volumeSlider.Value;
-            volumeProgressBar.Value = volumeSlider.Value;           
-        }
-       
+        }       
     }
 }
--- VRVControl/ViewModel/GeneralViewModel.cs
+++ VRVControl/ViewModel/GeneralViewModel.cs
@@ -1,12 +1,164 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Speech.Recognition;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
 
 namespace VRVControl.ViewModel
 {
-    class GeneralViewModel :BindableBase
+    class GeneralViewModel : BindableBase
     {
+        public GeneralViewModel()
+        {
+            IncVol = new RelayCommand(() => IncVolExecute());
+            DecVol = new RelayCommand(() => DecVolExecute());
+            Mute = new RelayCommand(() => MuteExecute());
+            EnableVoiceControl = new RelayCommand(() => EnableVoiceControlExecute());
+            DisableVoiceControl = new RelayCommand(() => DisableVoiceControlExecute());
+        }
+
+        // initialize speech recognition 
+        public SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        public Choices clist = new Choices();
+
+        // NAudio device enumerator
+        public MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
+
+        private bool _isEnableVoiceControlEnabled = true;
+
+        public bool IsEnableVoiceControlEnabled
+        {
+            get
+            {
+                return _isEnableVoiceControlEnabled;
+            }
+            set
+            {
+                _isEnableVoiceControlEnabled = value;
+                this.OnPropertyChanged("IsEnableVoiceControlEnabled");
+            }
+        }
+        private bool _isDisableVoiceControlEnabled = false;
+
+        public bool IsDisableVoiceControlEnabled
+        {
+            get
+            {
+                return _isDisableVoiceControlEnabled;
+            }
+            set
+            {
+                _isDisableVoiceControlEnabled = value;
+                this.OnPropertyChanged("IsDisableVoiceControlEnabled");
+            }
+        }
+
+        // Volume slider
+        private float _volume;
+        public float Volume
+        {
+            get { return this._volume; }
+            set
+            {
+                this._volume = value;
+
+                MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar = value;
+
+                this.OnPropertyChanged(nameof(this.Volume));
+            }
+        }       
+
+        public ICommand IncVol { get; private set; }
+
+        private void IncVolExecute()
+        {
+            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            defaultDevice.AudioEndpointVolume.VolumeStepUp();
+        }
+
+        public ICommand DecVol { get; private set; }
+
+        private void DecVolExecute()
+        {
+            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            defaultDevice.AudioEndpointVolume.VolumeStepDown();
+        }
+
+        public ICommand Mute { get; private set; }
+
+        private void MuteExecute()
+        {
+            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            var deviceState = defaultDevice.AudioEndpointVolume.Mute;
+
+            if(deviceState)
+            {
+                defaultDevice.AudioEndpointVolume.Mute = false;
+            }
+            else
+                defaultDevice.AudioEndpointVolume.Mute = true;
+        }
+
+        public ICommand EnableVoiceControl { get; private set; }
+
+        private void EnableVoiceControlExecute()
+        {
+            IsEnableVoiceControlEnabled = false;
+            IsDisableVoiceControlEnabled = true;
+
+            clist.Add(new string[] { "Increase sound", "Decrease sound", "Mute", "Close" });
+            Grammar gr = new Grammar(new GrammarBuilder(clist));
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+
+            switch (e.Result.Text.ToString())
+            {
+                case "Increase sound":
+                    defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar += (float)0.10;
+                    break;
+                case "Decrease sound":
+                    defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar -= (float)0.10;
+                    break;
+                case "Mute":
+                    MuteExecute();
+                    break;
+                case "Close":
+                    Application.Current.Shutdown();
+                    break;
+            }
+
+        }
+
+        public ICommand DisableVoiceControl { get; private set; }
+
+        private void DisableVoiceControlExecute()
+        {
+            sre.RecognizeAsyncStop();
+            IsEnableVoiceControlEnabled = true;
+            IsDisableVoiceControlEnabled = false;
+        }
     }
 }
