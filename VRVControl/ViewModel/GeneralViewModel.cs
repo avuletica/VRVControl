5c3a004db0003cd7a6d1c09b5a2ad9d44c90a07d
Bcoolie <vuletic23@gmail.com>
2016-06-11 17:43:02 +0200

    Implement timer for volume level

--- VRVControl/ViewModel/GeneralViewModel.cs
+++ VRVControl/ViewModel/GeneralViewModel.cs
@@ -1,13 +1,28 @@
 ﻿using NAudio.CoreAudioApi;
+using System;
+using System.Windows.Threading;
 
 namespace VRVControl.ViewModel
 {
     class GeneralViewModel : BindableBase
     {
         // NAudio device enumerator
-        public MMDeviceEnumerator devEnum = new MMDeviceEnumerator();      
+        public MMDeviceEnumerator devEnum = new MMDeviceEnumerator();
 
-        // Volume slider
+        public GeneralViewModel()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            MMDevice defaultDevice = devEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            Volume = defaultDevice.AudioEndpointVolume.MasterVolumeLevelScalar;
+        }          
+
         private float _volume;
         public float Volume
         {
@@ -22,6 +37,6 @@ namespace VRVControl.ViewModel
                 this.OnPropertyChanged(nameof(this.Volume));                
             }
         }       
-       
+
     }
 }
